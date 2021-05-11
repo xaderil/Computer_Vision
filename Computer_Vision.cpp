882dd8d789670793c66675f1e5bcd586a2abcd54
Maxim Skripnikov <hjbgbj@gmail.com>
2021-05-12 00:39:04 +1000

    Created class with graphs

--- Computer_Vision.cpp
+++ Computer_Vision.cpp
@@ -1,34 +1,25 @@
+#define FRME_HEIGHT 720
+#define FRME_WIDTH 1280
+#define COLOR "green"
+#define CUSTOMIZATION true
+#define NUM_OF_CAMERA 0
+#define FORECAST_DISTANCE 1000
+#define WINDOW_SIZE 30
+
 // Standard Libraries
 #include <stdint.h>
 #include <chrono>
 #include <thread>
 #include <fstream>
-// ROOT Libraries
-#include "TCanvas.h"
-#include "TGraph.h"
-#include "TMultiGraph.h"
-#include "TApplication.h"
-#include "TAxis.h"
-#include "TSystem.h"
-#include "TDatime.h"
-#include "TStyle.h"
-#include "TStopwatch.h"
 // nlohmann JSON
 #include "json.hpp"
 // Project Headers
 #include "Seeker.hpp"
 #include "Ball.hpp"
 #include "Forecaster.hpp"
+#include "Monitoring.hpp"
 
 using namespace std::chrono;
-
-constexpr int window_size = 30;
-constexpr double forecast_distance = 1000; // ms
-int NUM_OF_CAMERA = 0;
-bool CUSTOMIZATION = true;
-string COLOR = "green";
-int FRAME_WIDTH = 1280;
-int FRAME_HEIGHT = 720;
 
 int H_MIN = 0;
 int H_MAX = 256;
@@ -70,87 +61,22 @@ void Create_Customization() {
 };
 int main(int argc, char* argv[]) {
 
-     Forecaster::datasize = size_t(window_size);
-     Forecaster::forecast_distance = forecast_distance;
-     // Forecaster::resizeTimeForecast();
+     Forecaster::datasize = size_t(WINDOW_SIZE);
+     Forecaster::forecast_distance = FORECAST_DISTANCE;
+     
+     Monitoring Monitoring(argc, argv, FORECAST_DISTANCE);
 
-     // ROOT PLOTS INIT
-     TApplication rootapp("spectrum", &argc, argv);
      //set time offset
      chrono::milliseconds start_time_ms = chrono::duration_cast<chrono::milliseconds>(system_clock::now().time_since_epoch());
 
-     auto c1 = new TCanvas("c1", "Data");
-          c1->SetWindowSize(960, 960);
-          c1->SetGrid();
-    
-     auto mg1 = new TMultiGraph("X во времени","X from time");
-          mg1->GetXaxis()->SetTitle("Time");
-          mg1->GetYaxis()->SetTitle("X");
-          mg1->GetXaxis()->SetLimits(-(forecast_distance), forecast_distance);
+     double forecast_step = FORECAST_DISTANCE*2/FORECAST_DISTANCE;
 
-     auto mg2 = new TMultiGraph("Y во времени","Y from time");
-          mg2->GetXaxis()->SetTitle("Time");
-          mg2->GetYaxis()->SetTitle("Y");
-          mg2->GetXaxis()->SetLimits(-(forecast_distance), forecast_distance);
-
-     auto mg3 = new TMultiGraph("Z во времени","Z from time");
-          mg3->GetXaxis()->SetTitle("Time");
-          mg3->GetYaxis()->SetTitle("Z");
-          mg3->GetXaxis()->SetLimits(-(forecast_distance), forecast_distance);
-
-     auto f1 = new TGraph(2);
-          f1->SetName("Реальная координата X");
-          f1->SetLineColor(2);
-          f1->SetLineWidth(2);
-
-     auto f1_forecast = new TGraph(2);
-          f1_forecast->SetName("Прогнозная координата X");
-          f1_forecast->SetLineColor(4);
-          f1_forecast->SetLineWidth(2);
-
-     auto f2 = new TGraph(2);
-          f2->SetName("Реальная координата Y");
-          f2->SetLineColor(2);
-          f2->SetLineWidth(2);
-
-     auto f2_forecast = new TGraph(2);
-          f2_forecast->SetName("Прогнозная координата Y");
-          f2_forecast->SetLineColor(4);
-          f2_forecast->SetLineWidth(2);
-
-     auto f3 = new TGraph(2);
-          f3->SetName("Реальная координата Z");
-          f3->SetLineColor(2);
-          f3->SetLineWidth(2);
-
-     auto f3_forecast = new TGraph(2);
-          f3_forecast->SetName("Прогнозная координата Z");
-          f3_forecast->SetLineColor(4);
-          f3_forecast->SetLineWidth(2);
-     
-     double forecast_step = forecast_distance*2/window_size;
-
-     c1->Divide(1, 3);
-     mg1->Add(f1 );
-     c1->cd(1);
-     mg1->Add(f1_forecast);
-     mg1->Draw("AL");
-     c1->cd(2);
-     mg2->Add(f2_forecast);
-     mg2->Add(f2);
-     mg2->Draw("AL");
-     c1->cd(3);
-     mg3->Add(f3);
-     mg3->Add(f3_forecast);
-     mg3->Draw("AL");
-
-     TCanvas & cref = (*c1);
+     TCanvas & cref = *(Monitoring.c1);
      thread th(UPDATE_GRAPHS, ref(cref));
      th.detach();
 
      // Take properties of ball color
      Ball ball(COLOR);
-     //Ball* ptr = $ball;
      H_MIN = ball.getHSVmin()[0];
      H_MAX = ball.getHSVmax()[0];
      S_MIN = ball.getHSVmin()[1];
@@ -160,8 +86,8 @@ int main(int argc, char* argv[]) {
 
      VideoCapture video(0);
      video.set(CAP_PROP_FOURCC, VideoWriter::fourcc('M', 'J', 'P', 'G'));
-     video.set(CAP_PROP_FRAME_WIDTH, FRAME_WIDTH);
-     video.set(CAP_PROP_FRAME_HEIGHT, FRAME_HEIGHT);
+     video.set(CAP_PROP_FRAME_WIDTH, FRME_WIDTH);
+     video.set(CAP_PROP_FRAME_HEIGHT, FRME_HEIGHT);
 
      if (CUSTOMIZATION)
      {
@@ -175,8 +101,9 @@ int main(int argc, char* argv[]) {
           video.read(BGR_frame);
           cvtColor(BGR_frame, HSV_frame, COLOR_BGR2HSV);
           inRange(HSV_frame, Scalar(H_MIN, S_MIN, V_MIN), Scalar(H_MAX, S_MAX, V_MAX), Output_frame);
-          bool found = seeker.findObject(Out_ptr, BGR_ptr, FRAME_WIDTH, FRAME_HEIGHT);
+          bool found = seeker.findObject(Out_ptr, BGR_ptr, FRME_WIDTH, FRME_HEIGHT);
           if (found) {
+
                int pxX = seeker.get_pxX();
                int pxY = seeker.get_pxY();
                int pxDiameter = seeker.get_pxDiameter();
@@ -185,8 +112,8 @@ int main(int argc, char* argv[]) {
                ball.setYPos(pxY);
                ball.setZDiameter(pxDiameter);
                ball.calculateZRealPos();
-               ball.calculateXRealPos(FRAME_WIDTH, FRAME_HEIGHT);
-               ball.calculateYRealPos(FRAME_WIDTH, FRAME_HEIGHT);
+               ball.calculateXRealPos(FRME_WIDTH, FRME_HEIGHT);
+               ball.calculateYRealPos(FRME_WIDTH, FRME_HEIGHT);
 
                float xRealPos = ball.getXRealPos();
                float yRealPos = ball.getYRealPos();
@@ -215,19 +142,10 @@ int main(int argc, char* argv[]) {
                vector <double> ZData_Forecasted = Forecaster::getZData_Forecasted();
 
                vector <double> Time_Axis_Forecast = Forecaster::getTime_Data_Forecast();
- 
-               // cout << XData_Forecasted.size() << endl;
 
-               for(int i=0; i<XData.size() ;i++) {
-                    f1->SetPoint(i, Time_Axis[i]-current_time.count(), XData[i]*1000);
-                    f2->SetPoint(i, Time_Axis[i]-current_time.count(), YData[i]*1000);
-                    f3->SetPoint(i, Time_Axis[i]-current_time.count(), ZData[i]*1000);
-               };
-               // for(int i=0; i<XData_Forecasted.size() ;i++) {
-               //      f1_forecast->SetPoint(i, Time_Axis_Forecast[i]-current_time.count(), XData_Forecasted[i]*1000);
-               //      f2_forecast->SetPoint(i, Time_Axis_Forecast[i]-current_time.count(), YData_Forecasted[i]*1000);
-               //      f3_forecast->SetPoint(i, Time_Axis_Forecast[i]-current_time.count(), ZData_Forecasted[i]*1000);
-               // };
+               Monitoring.setPointsToCurrentData(XData, YData, ZData, Time_Axis, current_time);
+               Monitoring.setPointsToForecastedData(XData_Forecasted,YData_Forecasted, ZData_Forecasted, Time_Axis_Forecast, current_time);
+
 
                nlohmann::json j{};
                j["Time_Axis_Forecast"] = Time_Axis;
@@ -241,17 +159,8 @@ int main(int argc, char* argv[]) {
           } else {
 
                // Nullify all data
-               for(int i=Forecaster::num_of_chart_data; i>0 ;i--) {
-                    f1->RemovePoint(i);
-                    f2->RemovePoint(i);
-                    f3->RemovePoint(i);
-               };
                vector <double> XData_Forecasted = Forecaster::getXData_Forecasted();
-               for (int i = XData_Forecasted.size(); i > 0; i--) {
-                    f1_forecast->RemovePoint(i);
-                    f2_forecast->RemovePoint(i);
-                    f3_forecast->RemovePoint(i);
-               }
+               Monitoring.nullifyAllData(Forecaster::num_of_chart_data, XData_Forecasted.size());
                Forecaster::nullifyData();
           }
 
--- Monitoring.hpp
+++ Monitoring.hpp
@@ -1,8 +1,38 @@
 #pragma once
 
+#include <vector>
+#include <chrono>
+
+#include "TCanvas.h"
+#include "TGraph.h"
+#include "TMultiGraph.h"
+#include "TApplication.h"
+#include "TAxis.h"
+#include "TSystem.h"
+#include "TDatime.h"
+#include "TStyle.h"
+#include "TStopwatch.h"
+
+using namespace std;
 class Monitoring {
 
 public:        
-    Monitoring();
+ 
+    Monitoring(int argc, char* argv[], double forecast_distance);
 
+    void nullifyAllData(int current_size, int size_forecasted);
+    void setPointsToCurrentData(vector <double> XData, vector <double> YData, vector<double> ZData, vector <double> Time_Axis, chrono::duration<double, std::milli> current_time);
+    void setPointsToForecastedData(vector <double> XData_Forecasted, vector <double> YData_Forecasted, vector <double> ZData_Forecasted, vector <double> Time_Axis_Forecast, chrono::duration<double, std::milli> current_time);
+
+    TApplication* rootapp;
+    TCanvas* c1;
+    TMultiGraph* mg1;
+    TMultiGraph* mg2;
+    TMultiGraph* mg3;
+    TGraph* f1;
+    TGraph* f2;
+    TGraph* f3;
+    TGraph* f1_forecast;
+    TGraph* f2_forecast;
+    TGraph* f3_forecast;
 };
